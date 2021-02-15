08d58fa5ca9bd53707fe663555eee64736fa9d67
dakaras <dan.karagiannis@protonmail.ch>
2021-02-15 16:37:27 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
- 
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hallo</h1>
-      {/*Header */}
+      <Header/>
       {/*Home */}
 
     </div>
--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,26 @@ function Header() {
     return (
         <div className='header'>
             <img className = 'header_logo'
-            src = 'http://pngimg.com/uploads/amazon/amazon_PNG24.png'/>
+            src = 'http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
 
-            <div className='header_searchInput' type='text'>
-                {/* logo */}
+            <div className='header_search'>
+                <input className='header_searchInput' type='text'></input>
+            </div>
+
+            <div className='header_nav'>
+                <div className='header_option'>
+                    <span className='header_optionLineOne'>Hello, Guest</span>
+                </div>
+                <div className='header_option'>
+                    <span className='header_optionLineOne'> Returns</span>
+                    <span className='header_optionLineTwo'> & Orders</span>
+
+                </div>
+                <div className='header_option'>
+                    <span className='header_optionLineOne'>Your</span>
+                    <span className='header_optionLineTwo'>Prime</span>
+
+                </div>
             </div>
         </div>
     )
