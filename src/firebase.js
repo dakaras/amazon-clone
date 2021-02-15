d0dad4c5c39fac70d2e1c9d182cf27a22f70dff2
dakaras <dan.karagiannis@protonmail.ch>
2021-02-15 15:41:32 -0600

    added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC00sKqTBquZSdO88GT3RuvnrPuV3nZWYI",
+    authDomain: "clone-4ff0d.firebaseapp.com",
+    projectId: "clone-4ff0d",
+    storageBucket: "clone-4ff0d.appspot.com",
+    messagingSenderId: "1035584971029",
+    appId: "1:1035584971029:web:508807597be51f1df9091c",
+    measurementId: "G-V8PSNRCQ5X"
+};
