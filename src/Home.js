345e96e6e38887a5a1c412da54db4f92b2daf0c5
dakaras <dan.karagiannis@protonmail.ch>
2021-02-16 13:44:15 -0600

    added product.css

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
     return (
@@ -7,7 +8,7 @@ function Home() {
             <div className='home_container'>
                 < img className='home_image' src='https://store-images.s-microsoft.com/image/apps.16285.14618985536919905.552c0017-6644-49a8-8467-8f7b34ce0428.30ad6b05-16d9-4d5e-a242-43107708a16a?mode=scale&q=90&h=1080&w=1920'/>
                 <div className='home_row'>
-                    {/*Product*/}
+                    <Product/>
                     {/*Product*/}
                 </div>
                 <div className='home_row'>
--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import './product.css'
+import './Product.css'
 
 function Product() {
     return (
         <div className='product'>
-            
+            <div className='product_info'>
+                <p>The Day Is Now Far Spent</p>
+                <p className='product_price'>
+                    <small>$</small>
+                    <strong>16.44</strong>
+                </p>
+                <p className='product_rating'>
+                    <p>⭐</p>
+                </p>
+            </div>
+            < img src = 'https://images-na.ssl-images-amazon.com/images/I/51UYSL+IuPL._SX332_BO1,204,203,200_.jpg'/>
+            <button>Add to Basket</button>
         </div>
     )
 }
