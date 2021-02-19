7e877a5028c2a26b6a7f5de784644eba4f1b00b3
dakaras <dan.karagiannis@protonmail.ch>
2021-02-19 13:21:25 -0600

    added image ad to checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,14 @@ import './Checkout.css'
 function Checkout() {
     return (
         <div className='checkout'>
-            <h1>check out the Checkout</h1>
+            <div className='checkout_left'>
+                <img 
+                className = 'checkout_ad'
+                src='https://images-eu.ssl-images-amazon.com/images/G/31/IN-hq/2020/img/Prime/XCM_Manual_1500x350_1210306_1210306_IN_Prime_Deals_7a028a6d_aba0_4767_9510_e5e013c3a81f_jpg_LOWER_QL85_.jpg' />
+            </div>
+            <div className='checkout_title'>
+                <h2>Your Shopping Basket</h2>
+            </div>
         </div>
     )
 }
